85334538189ac4b99d65d3fc8e2bcf5b7a668791
Elisa <jc.alonsome@gmail.com>
2023-02-20 14:59:19 +0100

    uff

--- src/js/displayScreens.js
+++ src/js/displayScreens.js
@@ -1,30 +1,56 @@
-const allArticles = document.querySelectorAll(".article");
-const leftBar = document.getElementById("bar-left");
-const rightBar = document.getElementById("bar-right");
-const htmlCssGallery = document.getElementById("html-css-gallery");
+const allArticles = document.querySelectorAll('.article');
+const leftBar = document.getElementById('bar-left');
+const rightBar = document.getElementById('bar-right');
+const htmlCssGallery = document.getElementById('html-css-gallery');
+const jsAccordion = document.getElementById('javascript-accordion');
+const bar = document.querySelectorAll('.bar');
 
-const displayScreens = (data) => {
-  console.log(data);
-  allArticles.forEach((article) => {
-    leftBar.classList.remove("bar--right--show");
-    rightBar.classList.remove("bar--left--show");
-
-    if (article.id === data) {
-      console.log(article.id);
-      article.classList.add("article--show");
-    } else {
-      article.classList.remove("article--show");
-    }
+const openBars = () => {
+  console.log('hola');
+  bar.forEach(barElement => {
+    barElement.classList.add('bar--show');
   });
-  leftBar.classList.add("bar--right--show");
-  rightBar.classList.add("bar--left--show");
+  leftBar.addEventListener('transitionend', () => {
+    const timeoutId = setTimeout(() => {
+      leftBar.classList.add('bar--left--show');
+      clearTimeout(timeoutId);
+    }, 200);
+  });
+  rightBar.addEventListener('transitionend', () => {
+    const timeoutId = setTimeout(() => {
+      rightBar.classList.add('bar--right--show');
+      clearTimeout(timeoutId);
+    }, 200);
+  });
 };
 
-const displayWebButtons = (dataset) => {
-  if (dataset === "html") {
-    htmlCssGallery.classList.add("gallery__container--show");
+const displayScreens = data => {
+  console.log(data);
+  bar.forEach(barElement => {
+    barElement.classList.remove('bar--show');
+  });
+  leftBar.classList.remove('bar--left--show');
+  rightBar.classList.remove('bar--right--show');
+
+  allArticles.forEach(article => {
+    if (article.id === data) {
+      console.log(article.id);
+      article.classList.add('article--show');
+    } else {
+      article.classList.remove('article--show');
+    }
+  });
+
+  openBars();
+};
+
+const displayWebButtons = dataset => {
+  if (dataset === 'html') {
+    jsAccordion.classList.remove('accordion__container--show');
+    htmlCssGallery.classList.add('gallery__container--show');
   } else {
-    htmlCssGallery.classList.remove("gallery__container--show");
+    htmlCssGallery.classList.remove('gallery__container--show');
+    jsAccordion.classList.add('accordion__container--show');
   }
 };
 
