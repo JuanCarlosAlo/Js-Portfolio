b2d95401d2cdab07f9145b9b89c6378432caf649
JC <jc.alonsome@gmail.com>
2023-02-20 02:15:36 +0100

    smtng idk

--- src/js/displayScreens.js
+++ src/js/displayScreens.js
@@ -1,17 +1,31 @@
-const allArticles = document.querySelectorAll('.article');
-const leftBar = document.getElementById('bar-left');
-const rightBar = document.getElementById('bar-right');
+const allArticles = document.querySelectorAll(".article");
+const leftBar = document.getElementById("bar-left");
+const rightBar = document.getElementById("bar-right");
+const htmlCssGallery = document.getElementById("html-css-gallery");
 
-const displayScreens = data => {
+const displayScreens = (data) => {
   console.log(data);
-  allArticles.forEach(article => {
+  allArticles.forEach((article) => {
+    leftBar.classList.remove("bar--right--show");
+    rightBar.classList.remove("bar--left--show");
+
     if (article.id === data) {
       console.log(article.id);
-      article.classList.add('article--show');
-      leftBar.classList.add('bar--right--show');
-      rightBar.classList.add('bar--left--show');
-    } else article.classList.remove('article--show');
+      article.classList.add("article--show");
+    } else {
+      article.classList.remove("article--show");
+    }
   });
+  leftBar.classList.add("bar--right--show");
+  rightBar.classList.add("bar--left--show");
 };
 
-export { displayScreens };
+const displayWebButtons = (dataset) => {
+  if (dataset === "html") {
+    htmlCssGallery.classList.add("gallery__container--show");
+  } else {
+    htmlCssGallery.classList.remove("gallery__container--show");
+  }
+};
+
+export { displayScreens, displayWebButtons };
