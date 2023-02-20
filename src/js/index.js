adfe4b2a6afb0a0d390e06979c551d7392e7e391
JC <jc.alonsome@gmail.com>
2023-02-20 02:16:02 +0100

    idk

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,16 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
-import '../scss/styles.scss';
-import { displayScreens } from '../js/displayScreens.js';
+import "../scss/styles.scss";
+import { displayScreens, displayWebButtons } from "../js/displayScreens.js";
 
-const menu = document.getElementById('menu');
+const menu = document.getElementById("menu");
+const webButtons = document.getElementById("web-buttons");
 
-menu.addEventListener('click', e => {
+menu.addEventListener("click", (e) => {
   if (e.target.dataset.menu) {
     displayScreens(e.target.dataset.menu);
   }
+});
+
+webButtons.addEventListener("click", (e) => {
+  displayWebButtons(e.target.dataset.button);
 });
