48c488cc25e41d47edfd005a8b807c98d11c1f53
Elisa <jc.alonsome@gmail.com>
2023-02-21 14:57:27 +0100

    oki

--- src/js/displayScreens.js
+++ src/js/displayScreens.js
@@ -1,60 +1,61 @@
-const allArticles = document.querySelectorAll(".article");
-const leftBar = document.getElementById("bar-left");
-const rightBar = document.getElementById("bar-right");
-const htmlCssGallery = document.getElementById("html-css-gallery");
-const jsAccordion = document.getElementById("javascript-accordion");
-const bar = document.querySelectorAll(".bar");
-const webProjectTitle = document.getElementById("web-project-title");
+const allArticles = document.querySelectorAll('.article');
+const leftBar = document.getElementById('bar-left');
+const rightBar = document.getElementById('bar-right');
+const htmlCssGallery = document.getElementById('html-css-gallery');
+const jsAccordion = document.getElementById('javascript-accordion');
+const bar = document.querySelectorAll('.bar');
+const webProjectTitle = document.getElementById('web-project-title');
 
 const openBars = () => {
-  bar.forEach((barElement) => {
-    barElement.addEventListener("transitionend", () => {
+  bar.forEach(barElement => {
+    barElement.addEventListener('transitionend', () => {
       const timeoutId = setTimeout(() => {
-        barElement.classList.remove("bar--close");
+        barElement.classList.remove('bar--close');
         clearTimeout(timeoutId);
       }, 200);
     });
   });
-  leftBar.addEventListener("transitionend", () => {
+  leftBar.addEventListener('transitionend', () => {
     const timeoutId = setTimeout(() => {
-      leftBar.classList.remove("bar--left--close");
+      leftBar.classList.remove('bar--left--close');
       clearTimeout(timeoutId);
     }, 200);
   });
-  rightBar.addEventListener("transitionend", () => {
+  rightBar.addEventListener('transitionend', () => {
     const timeoutId = setTimeout(() => {
-      rightBar.classList.remove("bar--right--close");
+      rightBar.classList.remove('bar--right--close');
       clearTimeout(timeoutId);
     }, 200);
   });
 };
 
-const displayScreens = (data) => {
-  bar.forEach((barElement) => {
-    barElement.classList.add("bar--close");
+const displayScreens = data => {
+  bar.forEach(barElement => {
+    barElement.classList.add('bar--close');
   });
-  leftBar.classList.add("bar--left--close");
-  rightBar.classList.add("bar--right--close");
+  leftBar.classList.add('bar--left--close');
+  rightBar.classList.add('bar--right--close');
   openBars();
-  allArticles.forEach((article) => {
+  allArticles.forEach(article => {
     if (article.id === data) {
       console.log(article.id);
-      article.classList.add("article--show");
+      article.classList.add('article--show');
     } else {
-      article.classList.remove("article--show");
+      article.classList.remove('article--show');
     }
   });
 };
 
-const displayWebButtons = (dataset) => {
-  if (dataset === "html") {
-    jsAccordion.classList.remove("accordion__container--show");
-    htmlCssGallery.classList.add("gallery__container--show");
-    webProjectTitle.classList.add("web-project-title--show");
+const displayWebButtons = dataset => {
+  console.log(jsAccordion.children[0]);
+  if (dataset === 'html') {
+    jsAccordion.classList.remove('accordion--show');
+    htmlCssGallery.classList.add('gallery__container--show');
+    webProjectTitle.classList.add('web-project-title--show');
   } else {
-    htmlCssGallery.classList.remove("gallery__container--show");
-    jsAccordion.classList.add("accordion__container--show");
-    webProjectTitle.classList.remove("web-project-title--show");
+    htmlCssGallery.classList.remove('gallery__container--show');
+    jsAccordion.classList.add('accordion--show');
+    webProjectTitle.classList.remove('web-project-title--show');
   }
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,25 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
-import "../scss/styles.scss";
-import { displayScreens, displayWebButtons } from "../js/displayScreens.js";
+import '../scss/styles.scss';
+import { displayScreens, displayWebButtons } from '../js/displayScreens.js';
 
-const menu = document.getElementById("menu");
-const webButtons = document.getElementById("web-buttons");
+const menu = document.getElementById('menu');
+const webButtons = document.getElementById('web-buttons');
+const darkMode = document.getElementById('dark-mode');
 
-menu.addEventListener("click", (e) => {
+menu.addEventListener('click', e => {
   if (e.target.dataset.menu) {
     displayScreens(e.target.dataset.menu);
   }
 });
 
-webButtons.addEventListener("click", (e) => {
+webButtons.addEventListener('click', e => {
   displayWebButtons(e.target.dataset.button);
+});
+
+window.addEventListener('resize', e => {
+  if (window.matchMedia('(min-width: 768px)').matches) {
+    darkMode.textContent = 'DARK MODE';
+  } else {
+    darkMode.textContent = '';
+  }
 });
