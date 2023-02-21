887503d43a3f6ff856ee9c5d5e1ba2b5dde1e631
JC <jc.alonsome@gmail.com>
2023-02-21 10:40:04 +0100

    oki doki

--- src/js/displayScreens.js
+++ src/js/displayScreens.js
@@ -1,56 +1,60 @@
-const allArticles = document.querySelectorAll('.article');
-const leftBar = document.getElementById('bar-left');
-const rightBar = document.getElementById('bar-right');
-const htmlCssGallery = document.getElementById('html-css-gallery');
-const jsAccordion = document.getElementById('javascript-accordion');
-const bar = document.querySelectorAll('.bar');
+const allArticles = document.querySelectorAll(".article");
+const leftBar = document.getElementById("bar-left");
+const rightBar = document.getElementById("bar-right");
+const htmlCssGallery = document.getElementById("html-css-gallery");
+const jsAccordion = document.getElementById("javascript-accordion");
+const bar = document.querySelectorAll(".bar");
+const webProjectTitle = document.getElementById("web-project-title");
 
 const openBars = () => {
-  bar.forEach(barElement => {
-    barElement.classList.add('bar--show');
+  bar.forEach((barElement) => {
+    barElement.addEventListener("transitionend", () => {
+      const timeoutId = setTimeout(() => {
+        barElement.classList.remove("bar--close");
+        clearTimeout(timeoutId);
+      }, 200);
+    });
   });
-  leftBar.addEventListener('transitionend', () => {
+  leftBar.addEventListener("transitionend", () => {
     const timeoutId = setTimeout(() => {
-      leftBar.classList.add('bar--left--show');
+      leftBar.classList.remove("bar--left--close");
       clearTimeout(timeoutId);
     }, 200);
   });
-  rightBar.addEventListener('transitionend', () => {
+  rightBar.addEventListener("transitionend", () => {
     const timeoutId = setTimeout(() => {
-      rightBar.classList.add('bar--right--show');
+      rightBar.classList.remove("bar--right--close");
       clearTimeout(timeoutId);
     }, 200);
   });
 };
 
-const displayScreens = data => {
-  console.log(data);
-  bar.forEach(barElement => {
-    console.log('hola');
-    barElement.classList.remove('bar--show');
+const displayScreens = (data) => {
+  bar.forEach((barElement) => {
+    barElement.classList.add("bar--close");
   });
-  leftBar.classList.remove('bar--left--show');
-  rightBar.classList.remove('bar--right--show');
-
-  allArticles.forEach(article => {
+  leftBar.classList.add("bar--left--close");
+  rightBar.classList.add("bar--right--close");
+  openBars();
+  allArticles.forEach((article) => {
     if (article.id === data) {
       console.log(article.id);
-      article.classList.add('article--show');
+      article.classList.add("article--show");
     } else {
-      article.classList.remove('article--show');
+      article.classList.remove("article--show");
     }
   });
-
-  openBars();
 };
 
-const displayWebButtons = dataset => {
-  if (dataset === 'html') {
-    jsAccordion.classList.remove('accordion__container--show');
-    htmlCssGallery.classList.add('gallery__container--show');
+const displayWebButtons = (dataset) => {
+  if (dataset === "html") {
+    jsAccordion.classList.remove("accordion__container--show");
+    htmlCssGallery.classList.add("gallery__container--show");
+    webProjectTitle.classList.add("web-project-title--show");
   } else {
-    htmlCssGallery.classList.remove('gallery__container--show');
-    jsAccordion.classList.add('accordion__container--show');
+    htmlCssGallery.classList.remove("gallery__container--show");
+    jsAccordion.classList.add("accordion__container--show");
+    webProjectTitle.classList.remove("web-project-title--show");
   }
 };
 
