cca141d4803ef06c7c869d8d6a28935388968fe5
JC <jc.alonsome@gmail.com>
2023-02-23 10:23:43 +0100

    oki

--- src/js/index.js
+++ src/js/index.js
@@ -1,47 +1,49 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
 import {
   displayScreens,
   displayWebButtons,
-  displayJsItems
-} from '../js/displayScreens.js';
-import moon from '../assets/images/moon.svg';
+  displayJsItems,
+} from "../js/displayScreens.js";
+import moon from "../assets/images/moon.svg";
 
-const menu = document.getElementById('menu');
-const webButtons = document.getElementById('web-buttons');
-const darkMode = document.getElementById('dark-mode');
-const accordion = document.getElementById('accordion');
+const menu = document.getElementById("menu");
+const webButtons = document.getElementById("web-buttons");
+const darkMode = document.getElementById("dark-mode");
+const accordion = document.getElementById("accordion");
 
-menu.addEventListener('click', e => {
+displayScreens("intro");
+
+menu.addEventListener("click", (e) => {
   if (e.target.dataset.menu) {
     displayScreens(e.target.dataset.menu);
   }
 });
 
-webButtons.addEventListener('click', e => {
+webButtons.addEventListener("click", (e) => {
   displayWebButtons(e.target.dataset.button);
 });
 
-accordion.addEventListener('click', e => {
+accordion.addEventListener("click", (e) => {
   console.log(e.target.dataset.name);
   displayJsItems(e.target.dataset.name);
 });
 
-if (window.matchMedia('(min-width: 455px)').matches) {
-  darkMode.children[0].classList.add('dark-mode__text--show');
-  darkMode.children[1].classList.remove('dark-mode__img--show');
+if (window.matchMedia("(min-width: 455px)").matches) {
+  darkMode.children[0].classList.add("dark-mode__text--show");
+  darkMode.children[1].classList.remove("dark-mode__img--show");
 } else {
-  darkMode.children[0].classList.remove('dark-mode__text--show');
-  darkMode.children[1].classList.add('dark-mode__img--show');
+  darkMode.children[0].classList.remove("dark-mode__text--show");
+  darkMode.children[1].classList.add("dark-mode__img--show");
 }
 
-window.addEventListener('resize', e => {
+window.addEventListener("resize", (e) => {
   console.log(moon);
-  if (window.matchMedia('(min-width: 455px)').matches) {
-    darkMode.children[0].classList.add('dark-mode__text--show');
-    darkMode.children[1].classList.remove('dark-mode__img--show');
+  if (window.matchMedia("(min-width: 455px)").matches) {
+    darkMode.children[0].classList.add("dark-mode__text--show");
+    darkMode.children[1].classList.remove("dark-mode__img--show");
   } else {
-    darkMode.children[0].classList.remove('dark-mode__text--show');
-    darkMode.children[1].classList.add('dark-mode__img--show');
+    darkMode.children[0].classList.remove("dark-mode__text--show");
+    darkMode.children[1].classList.add("dark-mode__img--show");
   }
 });
