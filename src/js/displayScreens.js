3aae1f1f5440a5079524010028bc057b7a61bfc9
JC <jc.alonsome@gmail.com>
2023-02-25 14:20:18 +0100

    idk

--- src/js/displayScreens.js
+++ src/js/displayScreens.js
@@ -1,85 +1,130 @@
-const allArticles = document.querySelectorAll('.article');
-const leftBar = document.getElementById('bar-left');
-const rightBar = document.getElementById('bar-right');
-const htmlCssGallery = document.getElementById('html-css-gallery');
-const jsAccordion = document.getElementById('javascript-accordion');
-const bar = document.querySelectorAll('.bar');
-const webProjectTitle = document.getElementById('web-project-title');
-const allAccrodionItems = document.querySelectorAll('.accordion__item');
+const allArticles = document.querySelectorAll(".article");
+const leftBar = document.getElementById("bar-left");
+const rightBar = document.getElementById("bar-right");
+const htmlCssGallery = document.getElementById("html-css-gallery");
+const jsAccordion = document.getElementById("javascript-accordion");
+const bar = document.querySelectorAll(".bar");
+const webProjectTitle = document.getElementById("web-project-title");
+const allAccrodionItems = document.querySelectorAll(".accordion__item");
 const rootStyles = document.documentElement.style;
+const allGalleryItems = document.querySelectorAll(".gallery__item");
+
+const allGalleryItemsArray = [...allGalleryItems];
 
 let counter = 0;
 
 const openBars = () => {
-  bar.forEach(barElement => {
-    barElement.addEventListener('transitionend', () => {
+  bar.forEach((barElement) => {
+    barElement.addEventListener("transitionend", () => {
       const timeoutId = setTimeout(() => {
-        barElement.classList.remove('bar--close');
+        barElement.classList.remove("bar--close");
         clearTimeout(timeoutId);
       }, 200);
     });
   });
-  leftBar.addEventListener('transitionend', () => {
+  leftBar.addEventListener("transitionend", () => {
     const timeoutId = setTimeout(() => {
-      leftBar.classList.remove('bar--left--close');
+      leftBar.classList.remove("bar--left--close");
       clearTimeout(timeoutId);
     }, 200);
   });
-  rightBar.addEventListener('transitionend', () => {
+  rightBar.addEventListener("transitionend", () => {
     const timeoutId = setTimeout(() => {
-      rightBar.classList.remove('bar--right--close');
+      rightBar.classList.remove("bar--right--close");
       clearTimeout(timeoutId);
     }, 200);
   });
 };
 
-const displayScreens = data => {
-  jsAccordion.classList.remove('accordion--show');
-  htmlCssGallery.classList.remove('gallery__container--show');
-  webProjectTitle.classList.remove('web-project-title--show');
-  bar.forEach(barElement => {
-    barElement.classList.add('bar--close');
+const displayScreens = (data) => {
+  jsAccordion.classList.remove("accordion--show");
+  htmlCssGallery.classList.remove("gallery__container--show");
+  webProjectTitle.classList.remove("web-project-title--show");
+  bar.forEach((barElement) => {
+    barElement.classList.add("bar--close");
   });
-  leftBar.classList.add('bar--left--close');
-  rightBar.classList.add('bar--right--close');
+  leftBar.classList.add("bar--left--close");
+  rightBar.classList.add("bar--right--close");
   openBars();
-  allArticles.forEach(article => {
+  allArticles.forEach((article) => {
     if (article.id === data) {
       console.log(article.id);
-      article.classList.add('article--show');
+      article.classList.add("article--show");
     } else {
-      article.classList.remove('article--show');
+      article.classList.remove("article--show");
     }
   });
 };
 
-const displayJsItems = data => {
-  allAccrodionItems.forEach(item => {
+const displayJsItems = (data) => {
+  allAccrodionItems.forEach((item) => {
     console.log(data, item.id);
     if (data === item.id) {
-      item.classList.add('accordion__item--show');
+      item.classList.add("accordion__item--show");
     } else {
-      item.classList.remove('accordion__item--show');
+      item.classList.remove("accordion__item--show");
     }
   });
 };
 
-const displayWebButtons = dataset => {
-  console.log(jsAccordion.children[0]);
-  if (dataset === 'html') {
-    jsAccordion.classList.remove('accordion--show');
-    htmlCssGallery.classList.add('gallery__container--show');
-    webProjectTitle.classList.add('web-project-title--show');
+const displayWebButtons = (dataset, arrowRight, arrowLeft) => {
+  if (dataset === "html") {
+    arrowRight.classList.add("gallery__arrow--right--show");
+    arrowLeft.classList.add("gallery__arrow--left--show");
+    jsAccordion.classList.remove("accordion--show");
+    htmlCssGallery.classList.add("gallery__container--show");
+    webProjectTitle.classList.add("web-project-title--show");
+    allAccrodionItems.forEach((item) => {
+      item.classList.remove("accordion__item--show");
+    });
   } else {
-    htmlCssGallery.classList.remove('gallery__container--show');
-    jsAccordion.classList.add('accordion--show');
-    webProjectTitle.classList.remove('web-project-title--show');
+    arrowRight.classList.remove("gallery__arrow--right--show");
+    arrowLeft.classList.remove("gallery__arrow--left--show");
+    htmlCssGallery.classList.remove("gallery__container--show");
+    jsAccordion.classList.add("accordion--show");
+    webProjectTitle.classList.remove("web-project-title--show");
   }
 };
 
-const nextSlider = () => {
-  counter++;
-  rootStyles.setProperty('--slider-translate', counter * -100 + '%');
+const titleChange = () => {
+  const titleArray = [
+    "SunnySide",
+    "Clipboard",
+    "Blogger",
+    "Huddle",
+    "Insure",
+    "Loopstudios",
+  ];
+
+  webProjectTitle.textContent = titleArray[counter];
 };
 
-export { displayScreens, displayWebButtons, displayJsItems, nextSlider };
+const prevSlider = () => {
+  if (counter !== 0) {
+    counter--;
+    rootStyles.setProperty("--slider-translate", counter * -100 + "%");
+  } else {
+    counter = allGalleryItemsArray.length - 1;
+    rootStyles.setProperty("--slider-translate", counter * -100 + "%");
+  }
+  titleChange();
+};
+
+const nextSlider = () => {
+  if (allGalleryItemsArray.length - 2 >= counter) {
+    counter++;
+    rootStyles.setProperty("--slider-translate", counter * -100 + "%");
+  } else {
+    counter = 0;
+    rootStyles.setProperty("--slider-translate", counter);
+  }
+  titleChange();
+};
+
+export {
+  displayScreens,
+  displayWebButtons,
+  displayJsItems,
+  nextSlider,
+  prevSlider,
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,56 +1,64 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
 import {
   displayScreens,
   displayWebButtons,
   displayJsItems,
-  nextSlider
-} from '../js/displayScreens.js';
-import moon from '../assets/images/moon.svg';
+  nextSlider,
+  prevSlider,
+} from "../js/displayScreens.js";
+import moon from "../assets/images/moon.svg";
 
-const menu = document.getElementById('menu');
-const webButtons = document.getElementById('web-buttons');
-const darkMode = document.getElementById('dark-mode');
-const accordion = document.getElementById('accordion');
-const galleryButtonNext = document.getElementById('arrow-right');
-const galleryButtonPrev = document.getElementById('arrow-left');
+const menu = document.getElementById("menu");
+const webButtons = document.getElementById("web-buttons");
+const darkMode = document.getElementById("dark-mode");
+const accordion = document.getElementById("accordion");
+const galleryButtonNext = document.getElementById("arrow-right");
+const galleryButtonPrev = document.getElementById("arrow-left");
 
-displayScreens('intro');
+displayScreens("intro");
 
-menu.addEventListener('click', e => {
+menu.addEventListener("click", (e) => {
   if (e.target.dataset.menu) {
     displayScreens(e.target.dataset.menu);
   }
 });
 
-webButtons.addEventListener('click', e => {
-  displayWebButtons(e.target.dataset.button);
+webButtons.addEventListener("click", (e) => {
+  displayWebButtons(
+    e.target.dataset.button,
+    galleryButtonNext,
+    galleryButtonPrev
+  );
 });
 
-accordion.addEventListener('click', e => {
+accordion.addEventListener("click", (e) => {
   console.log(e.target.dataset.name);
   displayJsItems(e.target.dataset.name);
 });
 
-galleryButtonNext.addEventListener('click', e => {
+galleryButtonNext.addEventListener("click", (e) => {
   nextSlider();
 });
+galleryButtonPrev.addEventListener("click", (e) => {
+  prevSlider();
+});
 
-if (window.matchMedia('(min-width: 455px)').matches) {
-  darkMode.children[0].classList.add('dark-mode__text--show');
-  darkMode.children[1].classList.remove('dark-mode__img--show');
+if (window.matchMedia("(min-width: 455px)").matches) {
+  darkMode.children[0].classList.add("dark-mode__text--show");
+  darkMode.children[1].classList.remove("dark-mode__img--show");
 } else {
-  darkMode.children[0].classList.remove('dark-mode__text--show');
-  darkMode.children[1].classList.add('dark-mode__img--show');
+  darkMode.children[0].classList.remove("dark-mode__text--show");
+  darkMode.children[1].classList.add("dark-mode__img--show");
 }
 
-window.addEventListener('resize', e => {
+window.addEventListener("resize", (e) => {
   console.log(moon);
-  if (window.matchMedia('(min-width: 455px)').matches) {
-    darkMode.children[0].classList.add('dark-mode__text--show');
-    darkMode.children[1].classList.remove('dark-mode__img--show');
+  if (window.matchMedia("(min-width: 455px)").matches) {
+    darkMode.children[0].classList.add("dark-mode__text--show");
+    darkMode.children[1].classList.remove("dark-mode__img--show");
   } else {
-    darkMode.children[0].classList.remove('dark-mode__text--show');
-    darkMode.children[1].classList.add('dark-mode__img--show');
+    darkMode.children[0].classList.remove("dark-mode__text--show");
+    darkMode.children[1].classList.add("dark-mode__img--show");
   }
 });
