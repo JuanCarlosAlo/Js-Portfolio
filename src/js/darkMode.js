979e77a53d29e2603d029c7ec889da2f9ba1884c
JC <jc.alonsome@gmail.com>
2023-03-16 15:41:56 +0100

    oki

--- src/js/darkMode.js
+++ src/js/darkMode.js
@@ -1,6 +1,6 @@
 const darkModeToggle = (darkMode, sun, moon) => {
   document.body.classList.toggle("body-dark");
-  document.body.classList.toggle("body-dark + svg");
+
   if (document.body.classList.contains("body-dark")) {
     darkMode.children[1].src = moon;
     darkMode.children[0].textContent = "LIGHT";
